2375abc9585d00d81a9ba8fecdf496fa7f4cbae2
Drag-Sama <dragsama6@gmail.com>
2025-03-10 10:37:59 +0100

    Mise en place classe Points

--- Projet LOO2/src/Point/Point.java
+++ Projet LOO2/src/Point/Point.java
@@ -1,5 +1,27 @@
 package Point;
 
 public class Point {
+    float x;
+    float y;
 
+    public void setX(int newX){
+        this.x = newX;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public void setY(int newY){
+        this.y = newY;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    float getDist(Point a){
+        return Math.abs(this.x - a.getX() + this.y - a.getY());
+    }
 }
+
