374efbac047c94e2ff20fa932177bb2563f85b6c
Drag-Sama <dragsama6@gmail.com>
2025-03-26 13:42:40 +0100

    Plan

--- Projet LOO2/src/Plan/Plan.java
+++ Projet LOO2/src/Plan/Plan.java
@@ -27,34 +27,66 @@ public class Plan {
 
     ///////// ACCESSEURS /////////////
 
+    /**
+     * Renvoie les formes du plan
+     * @return Les formes
+     */
     public LinkedHashSet<Formes> getFormes() {
         return formes;
     }
 
+    /**
+     * Renvoie les points du plan
+     * @return Les points
+     */
     public LinkedHashSet<Point> getPoints() {
         return points;
     }
 
+    /**
+     * Renvoie le nombre de formes dans le plan
+     * @return Le nombre de formes
+     */
     public int getNbFormes() {
         return nbFormes;
     }
 
+    /**
+     * Renvoie le nombre de points dans le plan
+     * @return Le nombre de points
+     */
     public int getNbPoints() {
         return nbPoints;
     }
 
+    /**
+     * Permet de changer les formes du plan
+     * @param formes Les nouvelles formes
+     */
     public void setFormes(LinkedHashSet<Formes> formes) {
         this.formes = formes;
     }
 
+    /**
+     * Permet de changer les points du plan
+     * @param points Les nouveaux points du plan
+     */
     public void setPoints(LinkedHashSet<Point> points) {
         this.points = points;
     }
 
+    /**
+     * Permet de changer le nombre de formes dans le plan
+     * @param nbFormes Nouveau nombre de formes
+     */
     public void setNbFormes(int nbFormes) {
         this.nbFormes = nbFormes;
     }
 
+    /**
+     * Permet de changer le nombre de points dans le plan
+     * @param nbPoints Nouveau nombre de points
+     */
     public void setNbPoints(int nbPoints) {
         this.nbPoints = nbPoints;
     }
